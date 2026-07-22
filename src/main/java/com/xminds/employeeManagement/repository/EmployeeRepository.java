ae610046d814c8598de972082d487f3254648ee8
Athira-xminds <athirababuraj@xminds.com>
2026-07-22 10:29:48 +0530

    Add custom queries to EmployeeRepository

--- src/main/java/com/xminds/employeeManagement/repository/EmployeeRepository.java
+++ src/main/java/com/xminds/employeeManagement/repository/EmployeeRepository.java
@@ -2,8 +2,25 @@ package com.xminds.employeeManagement.repository;
 
 import com.xminds.employeeManagement.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Query("SELECT e FROM Employee e WHERE e.salary > :salary")
+    List<Employee> findEmployeesWithSalaryGreaterThan(@Param("salary") Double salary);
+
+    List<Employee> findBySalaryBetween(Double minSalary, Double maxSalary);
+
+    List<Employee> findByEmployeeNameContainingIgnoreCase(String searchText);
+
+    @Query("SELECT e FROM Employee e WHERE e.department.id = :deptId AND e.salary > :minSalary")
+    List<Employee> findEmployeesByDeptAndMinSalary(@Param("deptId") Long deptId,
+                                                   @Param("minSalary") Double minSalary);
+    @Query("SELECT e FROM Employee e JOIN e.department d WHERE d.name = :deptName")
+    List<Employee> findEmployeesByDepartmentName(@Param("deptName") String deptName);
+
 }
